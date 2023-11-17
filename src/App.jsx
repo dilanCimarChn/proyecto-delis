28e66a38e80a22c22221847ef085d95971c143da
Dante Yupanqui <dantepapitorey.yr@gmail.com>
2023-11-17 01:37:37 -0400

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './components/home'
+
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 
   return (
     
+    <Home home/>
   )
 }
 
