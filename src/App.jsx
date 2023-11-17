f9506f904b9c80559955b31ab3ed005d5983f2e1
dilanCimarChn <2002dilanchoque@gmail.com>
2023-11-17 03:18:06 -0400

    Guardar cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-
-import Barra from './components/barra'
-import SelectPP from './components/selectPP'
+import { useState } from 'react';
+import Home from './components/home';
+import Barra from './components/barra';
+import SelectPP from './components/selectPP';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <Home/>
       <Barra />
       <SelectPP />
     </>
