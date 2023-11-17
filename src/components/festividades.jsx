a5938b97276e3fe6affcea5fedc07b0c027bdba0
Laura Flores <apazalau15@gmail.com>
2023-11-17 03:01:07 -0400

    anuncios

---
+++ src/components/festividades.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '/styles/festividad.css';
+
+function Home(props) {
+    const more = props.more;
+  
+    return (
+      <section> </section>
+    );
+  }
+  
+  export default Home;
+
