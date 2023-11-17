d78e112ed5202b0f2f6fb97b2916d5e114511bfc
william <sirjhoswilliam5@gmail.com>
2023-11-17 02:45:36 -0400

    grande

--- src/components/selectPP.jsx
+++ src/components/selectPP.jsx
@@ -4,8 +4,8 @@ function SelectPP() {
     return ( 
         <>
             <div className="button-container">
-                <button onclick="toggleColor(1)" className="button">Botón 1</button>
-                <button onclick="toggleColor(2)" className="button">Botón 2</button>
+                <button onclick="toggleColor(1)" className="button1">Pasteles</button>
+                <button onclick="toggleColor(2)" className="button2">Postres</button>
             </div> <script src="script.js"></script>
         </>
     )
